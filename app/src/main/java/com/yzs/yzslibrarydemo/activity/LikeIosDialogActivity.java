f879ce92fbb8d859d61837c98363b044a70476f6
yzs <541567595@qq.com>
2016-11-22 17:33:10 +0800

    添加仿ios的dialog模块

---
+++ app/src/main/java/com/yzs/yzslibrarydemo/activity/LikeIosDialogActivity.java
@@ -0,0 +1,257 @@
+package com.yzs.yzslibrarydemo.activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.yzs.yzslibrary.base.YzsBaseActivity;
+import com.yzs.yzslibrary.entity.EventCenter;
+import com.yzs.yzslibrary.view.ActionSheetDialog;
+import com.yzs.yzslibrary.view.AlertDialog;
+import com.yzs.yzslibrarydemo.R;
+
+/**
+ * 创建人：姚智胜
+ * 创建时间： 2016/11/22.
+ * 简介：
+ */
+
+public class LikeIosDialogActivity extends YzsBaseActivity implements View.OnClickListener{
+
+    private Button btn1;
+    private Button btn2;
+    private Button btn3;
+    private Button btn4;
+    private Button btn5;
+    @Override
+    protected void initContentView(Bundle bundle) {
+        setContentView(R.layout.ac_like_ios_dialog);
+    }
+
+    @Override
+    protected void initView() {
+        btn1 = (Button) findViewById(R.id.btn1);
+
+        btn2 = (Button) findViewById(R.id.btn2);
+
+        btn3 = (Button) findViewById(R.id.btn3);
+
+        btn4 = (Button) findViewById(R.id.btn4);
+
+        btn5 = (Button) findViewById(R.id.btn5);
+
+    }
+
+    @Override
+    protected void initLogic() {
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+    }
+
+    @Override
+    protected void getBundleExtras(Bundle extras) {
+
+    }
+
+    @Override
+    protected void onEventComing(EventCenter center) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn1:
+                new ActionSheetDialog(LikeIosDialogActivity.this)
+                        .builder()
+                        .setTitle("清空消息列表后，聊天记录依然保留，确定要清空消息列表？")
+                        .setCancelable(false)
+                        .setCanceledOnTouchOutside(false)
+                        .addSheetItem("清空消息列表", ActionSheetDialog.SheetItemColor.Red,
+                                new ActionSheetDialog.OnSheetItemClickListener() {
+                                    @Override
+                                    public void onClick(int which) {
+
+                                    }
+                                }).show();
+                break;
+            case R.id.btn2:
+                new ActionSheetDialog(LikeIosDialogActivity.this)
+                        .builder()
+                        .setCancelable(false)
+                        .setCanceledOnTouchOutside(false)
+                        .addSheetItem("发送给好友", ActionSheetDialog.SheetItemColor.Blue,
+                                new ActionSheetDialog.OnSheetItemClickListener() {
+                                    @Override
+                                    public void onClick(int which) {
+
+                                    }
+                                })
+                        .addSheetItem("转载到空间相册", ActionSheetDialog.SheetItemColor.Blue,
+                                new ActionSheetDialog.OnSheetItemClickListener() {
+                                    @Override
+                                    public void onClick(int which) {
+
+                                    }
+                                })
+                        .addSheetItem("上传到群相册", ActionSheetDialog.SheetItemColor.Blue,
+                                new ActionSheetDialog.OnSheetItemClickListener() {
+                                    @Override
+                                    public void onClick(int which) {
+
+                                    }
+                                })
+                        .addSheetItem("保存到手机", ActionSheetDialog.SheetItemColor.Blue,
+                                new ActionSheetDialog.OnSheetItemClickListener() {
+                                    @Override
+                                    public void onClick(int which) {
+
+                                    }
+                                })
+                        .addSheetItem("收藏", ActionSheetDialog.SheetItemColor.Blue,
+                                new ActionSheetDialog.OnSheetItemClickListener() {
+                                    @Override
+                                    public void onClick(int which) {
+
+                                    }
+                                })
+                        .addSheetItem("查看聊天图片", ActionSheetDialog.SheetItemColor.Blue,
+                                new ActionSheetDialog.OnSheetItemClickListener() {
+                                    @Override
+                                    public void onClick(int which) {
+
+                                    }
+                                }).show();
+                break;
+            case R.id.btn3:
+                new ActionSheetDialog(LikeIosDialogActivity.this)
+                        .builder()
+                        .setTitle("请选择操作")
+                        .setCancelable(false)
+                        .setCanceledOnTouchOutside(false)
+                        .addSheetItem("条目一", ActionSheetDialog.SheetItemColor.Blue,
+                                new ActionSheetDialog.OnSheetItemClickListener() {
+                                    @Override
+                                    public void onClick(int which) {
+                                        Toast.makeText(LikeIosDialogActivity.this,
+                                                "item" + which, Toast.LENGTH_SHORT)
+                                                .show();
+                                    }
+                                })
+                        .addSheetItem("条目二", ActionSheetDialog.SheetItemColor.Blue,
+                                new ActionSheetDialog.OnSheetItemClickListener() {
+                                    @Override
+                                    public void onClick(int which) {
+                                        Toast.makeText(LikeIosDialogActivity.this,
+                                                "item" + which, Toast.LENGTH_SHORT)
+                                                .show();
+                                    }
+                                })
+                        .addSheetItem("条目三", ActionSheetDialog.SheetItemColor.Blue,
+                                new ActionSheetDialog.OnSheetItemClickListener() {
+                                    @Override
+                                    public void onClick(int which) {
+                                        Toast.makeText(LikeIosDialogActivity.this,
+                                                "item" + which, Toast.LENGTH_SHORT)
+                                                .show();
+                                    }
+                                })
+                        .addSheetItem("条目四", ActionSheetDialog.SheetItemColor.Blue,
+                                new ActionSheetDialog.OnSheetItemClickListener() {
+                                    @Override
+                                    public void onClick(int which) {
+                                        Toast.makeText(LikeIosDialogActivity.this,
+                                                "item" + which, Toast.LENGTH_SHORT)
+                                                .show();
+                                    }
+                                })
+                        .addSheetItem("条目五", ActionSheetDialog.SheetItemColor.Blue,
+                                new ActionSheetDialog.OnSheetItemClickListener() {
+                                    @Override
+                                    public void onClick(int which) {
+                                        Toast.makeText(LikeIosDialogActivity.this,
+                                                "item" + which, Toast.LENGTH_SHORT)
+                                                .show();
+                                    }
+                                })
+                        .addSheetItem("条目六", ActionSheetDialog.SheetItemColor.Blue,
+                                new ActionSheetDialog.OnSheetItemClickListener() {
+                                    @Override
+                                    public void onClick(int which) {
+                                        Toast.makeText(LikeIosDialogActivity.this,
+                                                "item" + which, Toast.LENGTH_SHORT)
+                                                .show();
+                                    }
+                                })
+                        .addSheetItem("条目七", ActionSheetDialog.SheetItemColor.Blue,
+                                new ActionSheetDialog.OnSheetItemClickListener() {
+                                    @Override
+                                    public void onClick(int which) {
+                                        Toast.makeText(LikeIosDialogActivity.this,
+                                                "item" + which, Toast.LENGTH_SHORT)
+                                                .show();
+                                    }
+                                })
+                        .addSheetItem("条目八", ActionSheetDialog.SheetItemColor.Blue,
+                                new ActionSheetDialog.OnSheetItemClickListener() {
+                                    @Override
+                                    public void onClick(int which) {
+                                        Toast.makeText(LikeIosDialogActivity.this,
+                                                "item" + which, Toast.LENGTH_SHORT)
+                                                .show();
+                                    }
+                                })
+                        .addSheetItem("条目九", ActionSheetDialog.SheetItemColor.Blue,
+                                new ActionSheetDialog.OnSheetItemClickListener() {
+                                    @Override
+                                    public void onClick(int which) {
+                                        Toast.makeText(LikeIosDialogActivity.this,
+                                                "item" + which, Toast.LENGTH_SHORT)
+                                                .show();
+                                    }
+                                })
+                        .addSheetItem("条目十", ActionSheetDialog.SheetItemColor.Blue,
+                                new ActionSheetDialog.OnSheetItemClickListener() {
+                                    @Override
+                                    public void onClick(int which) {
+                                        Toast.makeText(LikeIosDialogActivity.this,
+                                                "item" + which, Toast.LENGTH_SHORT)
+                                                .show();
+                                    }
+                                }).show();
+                break;
+            case R.id.btn4:
+                new AlertDialog(LikeIosDialogActivity.this).builder().setTitle("退出当前账号")
+                        .setMsg("再连续登陆15天，就可变身为QQ达人。退出QQ可能会使你现有记录归零，确定退出？")
+                        .setPositiveButton("确认退出", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+
+                            }
+                        }).setNegativeButton("取消", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                }).show();
+                break;
+            case R.id.btn5:
+                new AlertDialog(LikeIosDialogActivity.this).builder()
+                        .setMsg("你现在无法接收到新消息提醒。请到系统-设置-通知中开启消息提醒")
+                        .setNegativeButton("确定", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+
+                            }
+                        }).show();
+                break;
+            default:
+                break;
+        }
+
+    }
+}
