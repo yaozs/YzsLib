d734e5e59ffbbfbf338a61030d897dda4b22e082
yaozs <541567595@qq.com>
2017-04-20 13:34:36 +0800

    修改utils运算bug，自己出错了。。。

--- yzslibrary/src/main/java/com/yzs/yzslibrary/util/ArithmeticUtils.java
+++ yzslibrary/src/main/java/com/yzs/yzslibrary/util/ArithmeticUtils.java
@@ -205,7 +205,7 @@ public class ArithmeticUtils {
             throw new IllegalArgumentException("The scale must be a positive integer or zero");
         }
         BigDecimal b1 = new BigDecimal(v1);
-        BigDecimal b2 = new BigDecimal(v1);
+        BigDecimal b2 = new BigDecimal(v2);
         return b1.divide(b2, scale, BigDecimal.ROUND_HALF_UP).toString();
     }
 
